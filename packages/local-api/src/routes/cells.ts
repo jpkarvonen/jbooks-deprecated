d7b13bef65408770130390d97ef6ece99aeadefd
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2022-02-28 12:44:54 -0500

    local api cells route setup:

---
+++ packages/local-api/src/routes/cells.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+  const fullPath = path.join(dir, filename);
+
+  router.get('/cells', async (req, res) => {
+    // Read the file
+
+    try {
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+      res.send(JSON.parse(result));
+    } catch (err: any) {
+      if (err.code === 'ENOENT') {
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post('/cells', async (req, res) => {
+    // Take the list of cells from the req object
+    // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+
+    // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
+  });
+
+  return router;
+};
